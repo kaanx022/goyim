b36fd3a1e83f304b9441841525a31732e5dff069
Kaan <kaan@myzeller.com>
2024-05-12 15:07:58 +1000

    Single headless emulator - 38

--- main.sh
+++ main.sh
@@ -5,6 +5,21 @@ echo "==========================================================================
 echo "Current Directory"
 pwd
 echo "============================================================================"
-echo "Directory Contents"
+echo "Directory Contents ./       (/home/runner/work/kaan/kaan)"
 ls -la
 echo "============================================================================"
+echo "Directory Contents ../      (/home/runner/work/kaan/"
+ls -la ../
+echo "============================================================================"
+echo "Directory Contents ../../   (/home/runner/work/)"
+ls -la ../../
+echo "============================================================================"
+echo "Directory Contents ../../../   (/home/runner/)"
+ls -la ../../../
+echo "============================================================================"
+echo "Directory Contents ../../../../   (/home/)"
+ls -la ../../../../
+echo "============================================================================"
+
+
+# wget a release file called "maestro_kaan.zip" from link github.com/sdfgsdfgd/maestro_kaan/releases/download/maestro_kaan/maestro_kaan.zip
